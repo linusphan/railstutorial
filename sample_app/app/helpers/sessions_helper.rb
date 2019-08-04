5e51d8f71beb82b252d080a0ea57a851cf56650d
Linus Phan <phanlinus@gmail.com>
2019-08-04 09:34:36 -0700

    add remember user feature

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -1,18 +1,29 @@
 module SessionsHelper
+  # logs in the given user
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  # remembers a user in a persisten session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # returns the current logged-in user (if any)
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
+  # returns true if the user is logged in, false otherwise
   def logged_in?
     !current_user.nil?
   end
 
+  # logs out the current user
   def log_out
     session.delete(:user_id)
     @current_user = nil
