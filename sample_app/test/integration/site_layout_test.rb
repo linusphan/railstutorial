74c16fb99e5cbfe911e7f4dc32c13e8af30524bf
Linus Phan <phanlinus@gmail.com>
2019-08-06 18:00:52 -0700

    add pagination and users index page

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -15,5 +15,26 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get signup_path
     assert_select 'title', full_title('Sign up')
+
+    # testing as signed in user
+    user = users(:linus)
+    log_in_as(user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(user)
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', signup_path
+
+
+    get users_path
+    assert_select 'title', full_title('All users')
+
+    get signup_path
+    assert_select 'title', full_title('Sign up')
   end
 end
